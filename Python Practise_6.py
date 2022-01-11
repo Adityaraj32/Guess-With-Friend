5d32e2143144b47c1ffc1d8a70f38e29889d815d
Aditya raj <87028110+Adityaraj32@users.noreply.github.com>
2022-01-11 22:04:42 +0530

    Add files via upload

--- Python Practise_6.py
+++ Python Practise_6.py
@@ -11,8 +11,8 @@ Player_2_num = int(input("Enter the number where you have to stop the guess: "))
 rand_num = random.randint(Player_1_num,Player_2_num)
 rand_num2 = random.randint(Player_1_num,Player_2_num)
 Time_Count = 0
-Time_Count2 = None
 def play_1():
+    Time_Count = 0
     while True:
         Time_Count += 1
         with open("Player_1.txt","w") as f:
@@ -20,14 +20,13 @@ def play_1():
         Player_1 = int(input("\nFriend Turn: Enter the number: "))
         if Player_1 == rand_num:
             print("Congraculations! You guess the correct number")
-            # break
-            exit()
         elif Player_1 > rand_num:
             print("Enter Smaller Number and try again")
         elif Player_1 < rand_num:
             print("Enter Larger Number and try again")
 
 def play_2():
+    Time_Count2 = 0
     while True:
         Time_Count2 += 1
         with open("Player_2.txt","w") as f:
@@ -35,23 +34,25 @@ def play_2():
         Player_2 = int(input("\nYour Turn: Enter the number: "))
         if Player_2 == rand_num2:
             print("Congraculations! You guess the correct number")
-            if Time_Count > Time_Count2:
-                print("You win!")
-            elif Time_Count < Time_Count2:
-                print("You Loose!")
-            # break
-            exit()
         elif Player_2 > rand_num2:
             print("Enter Smaller Number and try again")
         elif Player_2 < rand_num2:
             print("Enter Larger Number and try again")
-    
+def Win():    
+    if Time_Count > Time_Count2:
+        print("You win!")
+    elif Time_Count < Time_Count2:
+        print("You Loose!")
 rand = random.randint(1, 2)
 if rand == 1:
     play_1()
+    play_2()
+    exit()
 elif rand == 2:
     play_2()
-
+    play_1()
+    exit()
+Win()
 # while True:
 #     Time_Count += 1
 #     with open("Player_1.txt","w") as f:
